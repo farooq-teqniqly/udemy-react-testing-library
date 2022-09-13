40ed21a09c79c01df7b8e1b6f47851afd25d4266
Farooq Mahmud <mahmud.farooq@acuitybrands.com>
2022-09-13 08:31:48 -0700

    Refactor tests to use user-event

--- sundaes-on-demand/src/pages/summary/test/SummaryForm.test.js
+++ sundaes-on-demand/src/pages/summary/test/SummaryForm.test.js
@@ -1,35 +1,46 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
 import SummaryForm from "../SummaryForm";
 
+const setup = (jsx) => {
+  return {
+    user: userEvent.setup(),
+    ...render(jsx),
+  };
+};
+
 describe("SummaryForm", () => {
+  let testSetup = {};
+
+  beforeEach(() => {
+    testSetup = setup(<SummaryForm />);
+  });
+
   describe("When loaded", () => {
     test("Then the Terms and Conditions checkbox is not checked", () => {
-      render(<SummaryForm />);
       const cb = screen.getByRole("checkbox");
       expect(cb).not.toBeChecked();
     });
 
     test("And the Confirm Order button is disabled", () => {
-      render(<SummaryForm />);
       const button = screen.getByRole("button");
       expect(button).toBeDisabled();
     });
   });
 
   describe("When the Terms and Conditions checkbox is checked", () => {
-    test("Then the Confirm Order button is enabled", () => {
-      render(<SummaryForm />);
+    test("Then the Confirm Order button is enabled", async () => {
       const cb = screen.getByRole("checkbox");
-      fireEvent.click(cb);
+      await testSetup.user.click(cb);
       const button = screen.getByRole("button");
       expect(button).toBeEnabled();
     });
 
-    test("And checking the checkbox again disables the button", () => {
-      render(<SummaryForm />);
+    test("And checking the checkbox again disables the button", async () => {
       const cb = screen.getByRole("checkbox");
-      fireEvent.click(cb);
-      fireEvent.click(cb);
+      await testSetup.user.click(cb);
+      await testSetup.user.click(cb);
       const button = screen.getByRole("button");
       expect(button).toBeDisabled();
     });
